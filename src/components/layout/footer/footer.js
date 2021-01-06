e23c6d753e46c7939856b5dd5e55568f60fa8f97
pablo-rueda <pablors-j@hotmail.com>
2021-01-06 14:26:08 +0100

    footer

--- src/components/layout/footer/footer.js
+++ src/components/layout/footer/footer.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import footerStyles from "./footer.module.scss";
 
 const footer = () => {
     return ( 
-        <div>
-            <h1>Footer comp</h1>
+        <div className={footerStyles.container}>
+            <p className={footerStyles.text}>© 2021 Sofía Rodríguez - by Pablo Rueda</p>
         </div>
      );
 }
