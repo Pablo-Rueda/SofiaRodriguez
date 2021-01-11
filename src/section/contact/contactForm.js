a7a5d809e52a0c2cc15a93eb1e00f6ee1ef263e4
pablo-rueda <pablors-j@hotmail.com>
2021-01-11 16:40:37 +0100

    contact form validation / email sending

--- src/section/contact/contactForm.js
+++ src/section/contact/contactForm.js
@@ -3,19 +3,26 @@ import contactStyle from "./contact.module.scss"
 
 import Btn from "./../../components/button/button"
 
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
+import { Formik, Form, Field, ErrorMessage } from 'formik'; //simplify forms
+import * as Yup from 'yup'; // simplify information
+import emailjs from 'emailjs-com'; // manage email information
 
-const initialValues = {
+const initialValues = { //form information
     name: "",
     email: "",
     description:""
 };
-const onSubmit =  values => {
+const onSubmit =  (values,onSubmitProps) => {
     console.log("form data", values)
-    initialValues.name = "";
-    initialValues.email = "";
-    initialValues.description = "";
+    console.log("submit props", onSubmitProps)
+
+        // send the message via email with emailjs:
+    let serviceID = process.env.SERVICE_ID
+    let templateID = process.env.TEMPLATE_ID;
+    let userID = process.env.USER_ID;
+    emailjs.send(serviceID,templateID,values,userID) 
+
+    onSubmitProps.resetForm(); // clean form
 }
 
 const validationSchema = Yup.object({
@@ -31,37 +38,40 @@ const ContactForm = () => {
         initialValues={initialValues}
         onSubmit={onSubmit}
         validationSchema={validationSchema}>
-            <Form className={contactStyle.form}>
-                <Field className={contactStyle.field} placeholder="Nombre" 
-                    type="text" id="name" name="name" 
-                        /*
-                        Reduce with formik the form schema:
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur}
-                        value={formik.values.name} /> 
-                        Unused in Field formik component:
-                        {...formik.getFieldProps("name")} />
-                        */
-                        />
+            {formik => { 
+                return(
+                    <Form className={contactStyle.form}>
+                    <Field className={contactStyle.field} placeholder="Nombre" 
+                        type="text" id="name" name="name" 
+                            /*
+                            Reduce with formik the form schema:
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            value={formik.values.name} /> 
+                            Unused in Field formik component:
+                            {...formik.getFieldProps("name")} />
+                            */
+                            />
 
-                <ErrorMessage name="name" style={{color:"red"}}
-                        /*
-                        {formik.errors.name && formik.touched.name ? 
-                            <div className={contactStyle.error}> {formik.errors.name}</div>: 
-                            null}
-                        */
-                />
-                    
-                <Field className={contactStyle.field} placeholder="E-mail" 
-                        type="email" id="email" name="email" />
-                <ErrorMessage name="email" className={contactStyle.error} />
-                    
-                <Field as="textarea" placeholder="Motivo de consulta"  
-                    type="text" id="description" name="description" row="20"
-                    className={contactStyle.field} />
+                    <ErrorMessage name="name" style={{color:"red"}}
+                            /*
+                            {formik.errors.name && formik.touched.name ? 
+                                <div className={contactStyle.error}> {formik.errors.name}</div>: 
+                                null}
+                            */
+                    />
+                        
+                    <Field className={contactStyle.field} placeholder="E-mail" 
+                            type="email" id="email" name="email" />
+                    <ErrorMessage name="email" className={contactStyle.error} />
+                        
+                    <Field as="textarea" placeholder="Motivo de consulta"  
+                        type="text" id="description" name="description" row="20"
+                        className={contactStyle.field} />
 
-                <Btn btnColor="#FB68A2" hoverText="#FFFFFF" > Enviar</Btn>
-            </Form>
+                    <Btn btnColor="#FB68A2" hoverText="#FFFFFF" > Enviar</Btn>
+                </Form>
+            )}}
         </Formik>
      );
 }
